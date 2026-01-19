617b78811811418125ac150c6e1b825ffa31ad7d
Bruno Schmitt <brunosschmitt@gmail.com>
2026-01-19 11:58:39 -0300

    Adição dos Views

--- backend/orders/views.py
+++ backend/orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .models import Order
+from .serializers import OrderSerializer
+
+class ORderViewSet(ModelViewSet):
+    """
+    ViewSet responsável pelos endpoints de pedidos (Orders).
+
+    Regras:
+    - Apenas usuários autenticados acessam
+    - Usuário só enxerga os próprios pedidos
+    - Pedido criado sempre pertence ao usuário autenticado 
+    """
+
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        #Retorna os pedidos do usuário autenticado
+
+        return Order.objects.filter(user = self.request.user)
+    
+    def perform_create(self, serializer):
+        #Associa automaticamente o pedido ao usuário autenticado 
+
+        serializer.save(user=self.request.user)
